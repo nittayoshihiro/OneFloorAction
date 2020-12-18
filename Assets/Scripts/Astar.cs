24e3b56c53615e984a2850ef7b638d8b0269e540
nitta <yosshi3800@gmail.com>
2020-12-18 10:51:38 +0900

    簡易探索機能をa*scriptで作成しました。

--- Assets/Scripts/Astar.cs
+++ Assets/Scripts/Astar.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 
 public class Astar : MonoBehaviour
 {
@@ -6,7 +7,13 @@ public class Astar : MonoBehaviour
     NodeStatus[,] m_nodeStatus;
     Vector3[] m_openVector3s;
     Vector3[] m_closeVector3s;
+    Vector3[] m_route;
+    int m_moveCount=0;
 
+    Vector3[] RoutePos
+    {
+        get { return m_route; }
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +33,7 @@ public class Astar : MonoBehaviour
         AutoMappingV3 autoMappingV3 = canvasManager.GetComponent<AutoMappingV3>();
         m_mapStatus = autoMappingV3.GetMappingData;
 
+        //ノードの基礎データを作成
         m_nodeStatus = new NodeStatus[m_mapStatus.GetLength(0), m_mapStatus.GetLength(1)];
         for (int i = 0; i < m_mapStatus.GetLength(0); i++)
         {
@@ -36,6 +44,10 @@ public class Astar : MonoBehaviour
                 {
                     m_nodeStatus[i, j] = NodeStatus.None;
                 }
+                else
+                {
+                    m_nodeStatus[i, j] = NodeStatus.Wall;
+                }
             }
         }
 
@@ -43,25 +55,85 @@ public class Astar : MonoBehaviour
         m_nodeStatus[(int)startPosition.x, (int)startPosition.y] = NodeStatus.Start;
         m_nodeStatus[(int)goalPosition.x, (int)goalPosition.y] = NodeStatus.Goal;
 
+        //ムーブカウントを初期化してnodeを起動する
+        m_moveCount = 0;
+        Node(m_nodeStatus,startPosition,goalPosition,m_moveCount);
     }
 
-    void Node(NodeStatus[,] nodeStatuses, Vector3 startPosition, Vector3 goalPosition)
+    void Node(NodeStatus[,] nodeStatuses, Vector3 startPosition, Vector3 goalPosition,int moveCount)
     {
-        if (m_nodeStatus[(int)startPosition.x++, (int)startPosition.y++] == NodeStatus.None)
+        //実コスト
+        int moveUp = 0;
+        int moveDown = 0;
+        int moveRight = 0;
+        int moveLeft = 0;
+
+        //コストを出すための仮Pos
+        Vector3 pos;
+
+        //ゴールポジションだったら終了
+        if (m_nodeStatus[(int)startPosition.x, (int)startPosition.y++] == NodeStatus.Goal||
+            m_nodeStatus[(int)startPosition.x, (int)startPosition.y--] == NodeStatus.Goal||
+            m_nodeStatus[(int)startPosition.x++, (int)startPosition.y] == NodeStatus.Goal||
+            m_nodeStatus[(int)startPosition.x--, (int)startPosition.y] == NodeStatus.Goal)
         {
-            m_nodeStatus[(int)startPosition.x++, (int)startPosition.y++] = NodeStatus.Open;
+             
         }
-        if (m_nodeStatus[(int)startPosition.x++, (int)startPosition.y--] == NodeStatus.None)
+        else
         {
-            m_nodeStatus[(int)startPosition.x++, (int)startPosition.y--] = NodeStatus.Open;
-        }
-        if (m_nodeStatus[(int)startPosition.x--, (int)startPosition.y++] == NodeStatus.None)
-        {
-            m_nodeStatus[(int)startPosition.x--, (int)startPosition.y++] = NodeStatus.Open;
-        }
-        if (m_nodeStatus[(int)startPosition.x--, (int)startPosition.y--] == NodeStatus.None)
-        {
-            m_nodeStatus[(int)startPosition.x--, (int)startPosition.y--] = NodeStatus.Open;
+            //ルートPosのためにリサイズする
+            Array.Resize(ref m_route, moveCount);
+
+            //コスト計算
+            if (m_nodeStatus[(int)startPosition.x, (int)startPosition.y++] == NodeStatus.None)
+            {
+                m_nodeStatus[(int)startPosition.x, (int)startPosition.y++] = NodeStatus.Open;
+                pos = new Vector3((int)startPosition.x, (int)startPosition.y++, 0);
+                moveUp = moveCount + (int)Vector3.Distance(pos, goalPosition);
+            }
+            if (m_nodeStatus[(int)startPosition.x, (int)startPosition.y--] == NodeStatus.None)
+            {
+                m_nodeStatus[(int)startPosition.x, (int)startPosition.y--] = NodeStatus.Open;
+                pos = new Vector3((int)startPosition.x, (int)startPosition.y--, 0);
+                moveDown = moveCount + (int)Vector3.Distance(pos, goalPosition);
+            }
+            if (m_nodeStatus[(int)startPosition.x++, (int)startPosition.y] == NodeStatus.None)
+            {
+                m_nodeStatus[(int)startPosition.x++, (int)startPosition.y] = NodeStatus.Open;
+                pos = new Vector3((int)startPosition.x++, (int)startPosition.y, 0);
+                moveRight = moveCount + (int)Vector3.Distance(pos, goalPosition);
+            }
+            if (m_nodeStatus[(int)startPosition.x--, (int)startPosition.y] == NodeStatus.None)
+            {
+                m_nodeStatus[(int)startPosition.x--, (int)startPosition.y] = NodeStatus.Open;
+                pos = new Vector3((int)startPosition.x--, (int)startPosition.y, 0);
+                moveLeft = moveCount + (int)Vector3.Distance(pos, goalPosition);
+            }
+
+            //今のノードを閉める
+            m_nodeStatus[(int)startPosition.x, (int)startPosition.y] = NodeStatus.Close;
+
+            //コストが低いのだけ、再起関数
+            if (moveUp <= moveDown && moveUp <= moveRight && moveUp <= moveLeft)
+            {
+                m_route[moveCount] = new Vector3((int)startPosition.x, (int)startPosition.y++, 0);
+                Node(nodeStatuses, m_route[moveCount], goalPosition, moveCount++);
+            }
+            else if (moveDown <= moveUp && moveDown <= moveRight && moveDown <= moveLeft)
+            {
+                m_route[moveCount] = new Vector3((int)startPosition.x, (int)startPosition.y--, 0);
+                Node(nodeStatuses, m_route[moveCount], goalPosition, moveCount++);
+            }
+            else if (moveRight <= moveUp && moveRight <= moveDown && moveRight <= moveLeft)
+            {
+                m_route[moveCount] = new Vector3((int)startPosition.x++, (int)startPosition.y, 0);
+                Node(nodeStatuses, m_route[moveCount], goalPosition, moveCount++);
+            }
+            else if (moveLeft <= moveUp && moveLeft <= moveDown && moveLeft <= moveRight)
+            {
+                m_route[moveCount] = new Vector3((int)startPosition.x--, (int)startPosition.y, 0);
+                Node(nodeStatuses, m_route[moveCount], goalPosition, moveCount++);
+            }
         }
     }
 
@@ -71,6 +143,7 @@ public class Astar : MonoBehaviour
         Open,
         Close,
         Start,
-        Goal
+        Goal,
+        Wall
     }
 }
